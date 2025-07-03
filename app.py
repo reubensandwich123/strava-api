572a71356b501ef419b019bddc0a778f112ed6e5
reubensandwich123 <reubenseet@gmail.com>
2025-07-03 13:34:41 +0000

    Created auth callback domain route and html file

--- app.py
+++ app.py
@@ -4,6 +4,19 @@ from requests.exceptions import ConnectionError, HTTPError
 
 app = Flask(__name__)
 
+
+
+
+
+
+
+
+
+
 @app.route("/")
 def index():
     return render_template("index.html", greeting="Hello and welcome!")
+
+@app.route("/auth_callback_dmain")
+def auth_callback():
+    return render_template("auth_callback_domain.html")
