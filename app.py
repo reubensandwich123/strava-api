7e6a7f8d0982e87fef41d7b2ee7f2bcf71054436
reubensandwich123 <reubenseet@gmail.com>
2025-07-03 13:23:15 +0000

    added __name__

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, request, render_template
 import requests
 from requests.exceptions import ConnectionError, HTTPError
 
+app = Flask(__name__)
 
 @app.route("/")
 def index():
