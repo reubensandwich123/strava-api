d4b585574fe3e6358850790d979e980726f9f2a2
reubensandwich123 <reubenseet@gmail.com>
2025-07-04 04:17:22 +0000

    Added token expiry check and refresh token function

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from flask import Flask, request, render_template, redirect, session
 from flask_session import Session
 import requests
 from requests.exceptions import ConnectionError, HTTPError
+import time
 
 app = Flask(__name__)
 
@@ -46,6 +47,9 @@ def strava_callback():
 
     token_decoded = token.json()
     session["access_token"] = token_decoded["access_token"]
+    session["expires_at"] = token_decoded["expires_at"]
+    session["refresh_token"] = token_decoded["refresh_token"]
+    session["expires_in"] = token_decoded["expires_in"]
     if not token_decoded["access_token"]:
         return render_template("failure.html", message="failed to get access token")
     session["athlete_username"] = token_decoded["athlete"]["username"]
@@ -55,18 +59,55 @@ def strava_callback():
 
 @app.route("/stats", methods=["POST"])
 def stats():
-    id = session["athlete_id"]
-    token = session["access_token"]
-    try:
-        response = requests.get(f"https://www.strava.com/api/v3/athletes/{id}/stats", headers={f"Authorization": "Bearer {token}"})
+    result = check_expiry(session["expires_at"])
+    if (result == True):
+
+        id = session["athlete_id"]
+        token = session["access_token"]
+        try:
+            response = requests.get(f"https://www.strava.com/api/v3/athletes/{id}/stats", headers={"Authorization": f"Bearer {token}"})
+            response.raise_for_status()
+        except ConnectionError as connErr:
+            return render_template("failure.html", message=str(connErr)
+        except HTTPError as httpErr:
+            status = response.status_code
+            return render_template('failure.html', message=f"{httpErr} with error code {status}")
+        response = response.json()
+        run_totals = response["all_run_totals"]
+        ride_totals = response["all_ride_totals"]
+        swim_totals = response["all_swim_totals"]
+        return render_template('stats.html', run_total=run_totals, ride_total=ride_totals, swim_total=swim_totals)
+    else:
+        return render_template("failure.html", message=(str(result)))
+def check_expiry(expires_at):
+    refresh_token = session["refresh_token"]
+    if (expires_at < time.time()):
+        res = refresh(refresh_token)
+        if (res != 0):
+            return res
+        else:
+            return True
+    else:
+        return True
+        
+def refresh(refresh_token):
+    try: 
+        response = requests.post("https://www.strava.com/oauth/token", data={            
+            'client_id': CLIENT_ID,
+            'client_secret': CLIENT_SECRET,
+            'grant_type': 'refresh_token',
+            'refresh_token': refresh_token}, headers={'Accept' : 'application/json'})
         response.raise_for_status()
     except ConnectionError as connErr:
-        return render_template("failure.html", message=connErr)
+        return str(connErr)
     except HTTPError as httpErr:
-        status = response.status_code
-        return render_template('failure.html', message=f"{httpErr} with error code {status}")
+        status_code = response.status_code
+        message=f"{httpErr} with error code {status_code}"
+        return message
+        
     response = response.json()
-    run_totals = response["all_run_totals"]
-    ride_totals = response["all_ride_totals"]
-    swim_totals = response["all_swim_totals"]
-    return render_template('stats.html', run_total=run_totals, ride_total=ride_totals, swim_total=swim_totals)
+    session["access_token"] = response["access_token"]
+    session["refresh_token"] = response["refresh_token"]
+    session["expires_at"] = response["expires_at"]
+    session["expires_in"] = response["expires_in"]
+    return 0
